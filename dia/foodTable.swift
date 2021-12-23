75a1a23f938145bf3fdf2f314fa0bcf9a037fbba
Artem <amedi.ioakim@gmail.com>
2021-12-23 18:12:53 +0300

    23.12.2021

--- dia/foodTable.swift
+++ dia/foodTable.swift
@@ -19,6 +19,9 @@ class Anatomy{
         let worksheet1 = workbook_add_worksheet(workbook, "Приемы пищи")
         //Widen the first column to make the text clearer.
         worksheet_set_column(worksheet1, 0, 0, 20, nil)
+        let merge_format = workbook_add_format(workbook);
+        format_set_align(merge_format, UInt8(LXW_ALIGN_CENTER.rawValue));
+        format_set_align(merge_format, UInt8(LXW_ALIGN_VERTICAL_CENTER.rawValue));
         
         //Write some unformatted data.
         let tbl = getFoodRecords()
@@ -26,11 +29,27 @@ class Anatomy{
         formater.dateFormat = "dd.MM.yyyy"
         let formater1 = DateFormatter()
         formater1.dateFormat = "HH:mm"
+                
+        var listOfIndex: [Int] = []
+        for i in 0..<tbl.count {
+            listOfIndex.append(tbl[i].food.joined().count)
+        }
+        var listOfIndex1: [Int] = []
+        for i in 0..<tbl.count {
+            for i1 in 0..<tbl[i].foodType.count{
+                listOfIndex1.append(tbl[i].food[i1].count)
+            }
+        }
+  
         var d = 0
         var r = 0
         var r1 = 0
+        
+        print(listOfIndex)
+        print(listOfIndex1)
         for i in 0..<tbl.count {
-            worksheet_write_string(worksheet1, lxw_row_t(d), 0, formater.string(from: tbl[i].day), nil)
+            worksheet_merge_range(worksheet1, lxw_row_t(listOfIndex[0..<i+1].reduce(0,+)-listOfIndex[i]), 0, lxw_row_t(listOfIndex[0..<i+1].reduce(0,+)-1), 0, nil, nil);
+            worksheet_write_string(worksheet1, lxw_row_t(d), 0, formater.string(from: tbl[i].day), merge_format)
             d = d + tbl[i].food.joined().count
             for i1 in 0..<tbl[i].time.count {
                 worksheet_write_string(worksheet1, lxw_row_t(r1), 1, formater1.string(from: tbl[i].time[i1]), nil)
@@ -43,16 +62,7 @@ class Anatomy{
                 r += 1
             }
         }
-        var listOfIndex: [Int] = []
-        for i in 0..<tbl.count {
-            listOfIndex.append(tbl[i].food.joined().count)
-        }
-        var listOfIndex1: [Int] = []
-        for i in 0..<tbl.count {
-            for i1 in 0..<tbl[i].foodType.count{
-                listOfIndex1.append(tbl[i].food[i1].count)
-            }
-        }
+
         //Close the workbook, save the file and free any memory.
         let error = workbook_close(workbook)
         //Check if there was any error creating the xlsx file.
