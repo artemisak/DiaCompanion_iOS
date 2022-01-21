d05de0aefaaf13b6ce720fdd412b1cf04722dc70
Artem <amedi.ioakim@gmail.com>
2022-01-21 16:05:09 +0300

    21.01.2022

--- dia/excelManager.swift
+++ dia/excelManager.swift
@@ -14,6 +14,35 @@ struct FoodRecord {
     var foodType: String
     var food:  [String]
     var g: [String]
+    var carbo: [String]
+    var prot: [String]
+    var fat: [String]
+    var ec: [String]
+    var gi: [String]
+    var empty: [String]
+    var water: [String]
+    var nzhk: [String]
+    var hol: [String]
+    var pv: [String]
+    var zola: [String]
+    var na: [String]
+    var k: [String]
+    var ca: [String]
+    var mg: [String]
+    var p: [String]
+    var fe: [String]
+    var a: [String]
+    var b1: [String]
+    var b2: [String]
+    var rr: [String]
+    var c: [String]
+    var re: [String]
+    var kar: [String]
+    var mds: [String]
+    var kr: [String]
+    var te: [String]
+    var ok: [String]
+    var ne: [String]
 }
 
 struct TableRecord {
@@ -22,6 +51,35 @@ struct TableRecord {
     var foodType: [String]
     var food:  [[String]]
     var g: [[String]]
+    var carbo: [[String]]
+    var prot: [[String]]
+    var fat: [[String]]
+    var ec: [[String]]
+    var gi: [[String]]
+    var empty: [[String]]
+    var water: [[String]]
+    var nzhk: [[String]]
+    var hol: [[String]]
+    var pv: [[String]]
+    var zola: [[String]]
+    var na: [[String]]
+    var k: [[String]]
+    var ca: [[String]]
+    var mg: [[String]]
+    var p: [[String]]
+    var fe: [[String]]
+    var a: [[String]]
+    var b1: [[String]]
+    var b2: [[String]]
+    var rr: [[String]]
+    var c: [[String]]
+    var re: [[String]]
+    var kar: [[String]]
+    var mds: [[String]]
+    var kr: [[String]]
+    var te: [[String]]
+    var ok: [[String]]
+    var ne: [[String]]
 }
 
 func getFoodRecords() -> [TableRecord] {
@@ -47,59 +105,166 @@ func getFoodRecords() -> [TableRecord] {
         dateFormatter1.locale = Locale(identifier: "ru_RU")
         dateFormatter1.setLocalizedDateFormatFromTemplate("HH:mm")
         
+        let foodInfo = Table("food")
+        let foodN = Expression<String>("name")
+        let carbo = Expression<Double>("carbo")
+        let prot = Expression<Double>("prot")
+        let fat = Expression<Double>("fat")
+        let ec = Expression<Double>("ec")
+        let gi = Expression<Double>("gi")
+        let water = Expression<Double>("water")
+        let nzhk = Expression<Double>("nzhk")
+        let hol = Expression<Double>("hol")
+        let pv = Expression<Double>("pv")
+        let zola = Expression<Double>("zola")
+        let na = Expression<Double>("na")
+        let k = Expression<Double>("k")
+        let ca = Expression<Double>("ca")
+        let mg = Expression<Double>("mg")
+        let p = Expression<Double>("p")
+        let fe = Expression<Double>("fe")
+        let a = Expression<Double>("a")
+        let b1 = Expression<Double>("b1")
+        let b2 = Expression<Double>("b2")
+        let rr = Expression<Double>("rr")
+        let c = Expression<Double>("c")
+        let re = Expression<Double>("re")
+        let kar = Expression<Double>("kar")
+        let mds = Expression<Double>("mds")
+        let kr = Expression<Double>("kr")
+        let te = Expression<Double>("te")
+        let ok = Expression<Double>("ok")
+        let ne = Expression<Double>("ne")
+        
         for i in try db.prepare(foodTable.select(day,time,foodType,foodName,gram)){
-            record.append(FoodRecord(day: dateFormatter.date(from: i[day])!, time: dateFormatter1.date(from: i[time])!,foodType: i[foodType], food: [i[foodName]], g: [i[gram]]))
+            record.append(FoodRecord(day: dateFormatter.date(from: i[day])!, time: dateFormatter1.date(from: i[time])!,foodType: i[foodType], food: [i[foodName]], g: [i[gram]], carbo: [""], prot: [""], fat: [""], ec: [""], gi: [""], empty: [""], water: [""], nzhk: [""], hol: [""], pv: [""], zola: [""], na: [""], k: [""], ca: [""], mg: [""], p: [""], fe: [""], a:[""], b1: [""], b2: [""], rr: [""], c: [""], re: [""], kar: [""], mds: [""], kr: [""], te: [""], ok: [""], ne: [""]))
         }
         
-        record = record.sorted(by: {($0.day, $0.time, $0.foodType) < ($1.day, $1.time, $1.foodType)})
-        
-        var i = 1
-        while i < record.count {
-            if record[i].day == record[i-1].day && record[i].time == record[i-1].time && record[i].foodType == record[i-1].foodType {
-                record[i].food = record[i-1].food + record[i].food
-                record[i].g = record[i-1].g + record[i].g
-                record.remove(at: i-1)
-            } else {
-                i += 1
+        for i in 0..<record.count {
+            for i1 in try db.prepare(foodInfo.select(foodN, carbo, prot, fat, ec, gi, water, nzhk, hol, pv, zola, na, k, ca, mg, p, fe, a, b1, b2, rr, c, re, kar, mds, kr, te, ok, ne).filter(foodN == record[i].food[0])){
+                record[i].carbo = ["\(i1[carbo])"]
+                record[i].prot = ["\(i1[prot])"]
+                record[i].fat = ["\(i1[fat])"]
+                record[i].ec = ["\(i1[ec])"]
+                record[i].gi = ["\(i1[gi])"]
+                record[i].water = ["\(i1[water])"]
+                record[i].nzhk = ["\(i1[nzhk])"]
+                record[i].hol = ["\(i1[hol])"]
+                record[i].pv = ["\(i1[pv])"]
+                record[i].zola = ["\(i1[zola])"]
+                record[i].na = ["\(i1[na])"]
+                record[i].k = ["\(i1[k])"]
+                record[i].ca = ["\(i1[ca])"]
+                record[i].mg = ["\(i1[mg])"]
+                record[i].p = ["\(i1[p])"]
+                record[i].fe = ["\(i1[fe])"]
+                record[i].a = ["\(i1[a])"]
+                record[i].b1 = ["\(i1[b1])"]
+                record[i].b2 = ["\(i1[b2])"]
+                record[i].rr = ["\(i1[rr])"]
+                record[i].c = ["\(i1[c])"]
+                record[i].re = ["\(i1[re])"]
+                record[i].kar = ["\(i1[kar])"]
+                record[i].mds = ["\(i1[mds])"]
+                record[i].kr = ["\(i1[kr])"]
+                record[i].te = ["\(i1[te])"]
+                record[i].ok = ["\(i1[ok])"]
+                record[i].ne = ["\(i1[ne])"]
             }
         }
-        
-        table.append(TableRecord(day: record[0].day, time: [record[0].time], foodType: [record[0].foodType], food: [record[0].food], g: [record[0].g]))
-        record.remove(at: 0)
-        
-        while (record.count>0) {
-            if record[0].day == table[table.endIndex-1].day {
-                table[table.endIndex-1].time.append(record[0].time)
-                table[table.endIndex-1].foodType.append(record[0].foodType)
-                table[table.endIndex-1].food.append(record[0].food)
-                table[table.endIndex-1].g.append(record[0].g)
-                record.remove(at: 0)
-            } else {
-                table.append(TableRecord(day: record[0].day, time:[record[0].time], foodType: [record[0].foodType], food:[record[0].food], g:[record[0].g]))
-                record.remove(at: 0)
+
+        if record.count > 0 {
+            record = record.sorted(by: {($0.day, $0.time, $0.foodType) < ($1.day, $1.time, $1.foodType)})
+            
+            var i = 1
+            while i < record.count {
+                if record[i].day == record[i-1].day && record[i].time == record[i-1].time && record[i].foodType == record[i-1].foodType {
+                    record[i].food = record[i-1].food + record[i].food
+                    record[i].g = record[i-1].g + record[i].g
+                    record[i].carbo = record[i-1].carbo + record[i].carbo
+                    record[i].prot = record[i-1].prot + record[i].prot
+                    record[i].fat = record[i-1].fat + record[i].fat
+                    record[i].ec = record[i-1].ec + record[i].ec
+                    record[i].gi = record[i-1].gi + record[i].gi
+                    record[i].empty = record[i-1].empty + record[i].empty
+                    record[i].water = record[i-1].water + record[i].water
+                    record[i].nzhk = record[i-1].nzhk + record[i].nzhk
+                    record[i].hol = record[i-1].hol + record[i].hol
+                    record[i].pv = record[i-1].pv + record[i].pv
+                    record[i].zola = record[i-1].zola + record[i].zola
+                    record[i].na = record[i-1].na + record[i].na
+                    record[i].k = record[i-1].k + record[i].k
+                    record[i].ca = record[i-1].ca + record[i].ca
+                    record[i].mg = record[i-1].mg + record[i].mg
+                    record[i].p = record[i-1].p + record[i].p
+                    record[i].fe = record[i-1].fe + record[i].fe
+                    record[i].a = record[i-1].a + record[i].a
+                    record[i].b1 = record[i-1].b1 + record[i].b1
+                    record[i].b2 = record[i-1].b2 + record[i].b2
+                    record[i].rr = record[i-1].rr + record[i].rr
+                    record[i].c = record[i-1].c + record[i].c
+                    record[i].re = record[i-1].re + record[i].re
+                    record[i].kar = record[i-1].kar + record[i].kar
+                    record[i].mds = record[i-1].mds + record[i].mds
+                    record[i].kr = record[i-1].kr + record[i].kr
+                    record[i].te = record[i-1].te + record[i].te
+                    record[i].ok = record[i-1].ok + record[i].ok
+                    record[i].ne = record[i-1].ne + record[i].ne
+                    record.remove(at: i-1)
+                } else {
+                    i += 1
+                }
             }
+            
+            table.append(TableRecord(day: record[0].day, time: [record[0].time], foodType: [record[0].foodType], food: [record[0].food], g: [record[0].g], carbo: [record[0].carbo], prot: [record[0].prot], fat: [record[0].fat], ec: [record[0].ec], gi: [record[0].gi], empty: [record[0].empty], water: [record[0].water], nzhk: [record[0].nzhk], hol: [record[0].hol], pv: [record[0].pv], zola: [record[0].zola], na: [record[0].na], k: [record[0].k], ca: [record[0].ca], mg: [record[0].mg], p: [record[0].p], fe: [record[0].fe], a: [record[0].a], b1: [record[0].b1], b2: [record[0].b2], rr: [record[0].rr], c: [record[0].c], re: [record[0].re], kar: [record[0].kar], mds: [record[0].mds], kr: [record[0].kr], te: [record[0].te], ok: [record[0].ok], ne: [record[0].ne]))
+            record.remove(at: 0)
+            
+            while (record.count>0) {
+                if record[0].day == table[table.endIndex-1].day {
+                    table[table.endIndex-1].time.append(record[0].time)
+                    table[table.endIndex-1].foodType.append(record[0].foodType)
+                    table[table.endIndex-1].food.append(record[0].food)
+                    table[table.endIndex-1].g.append(record[0].g)
+                    table[table.endIndex-1].carbo.append(record[0].carbo)
+                    table[table.endIndex-1].prot.append(record[0].prot)
+                    table[table.endIndex-1].fat.append(record[0].fat)
+                    table[table.endIndex-1].ec.append(record[0].ec)
+                    table[table.endIndex-1].gi.append(record[0].gi)
+                    table[table.endIndex-1].empty.append(record[0].empty)
+                    table[table.endIndex-1].water.append(record[0].water)
+                    table[table.endIndex-1].nzhk.append(record[0].nzhk)
+                    table[table.endIndex-1].hol.append(record[0].hol)
+                    table[table.endIndex-1].pv.append(record[0].pv)
+                    table[table.endIndex-1].zola.append(record[0].zola)
+                    table[table.endIndex-1].na.append(record[0].na)
+                    table[table.endIndex-1].k.append(record[0].k)
+                    table[table.endIndex-1].ca.append(record[0].ca)
+                    table[table.endIndex-1].mg.append(record[0].mg)
+                    table[table.endIndex-1].p.append(record[0].p)
+                    table[table.endIndex-1].fe.append(record[0].fe)
+                    table[table.endIndex-1].a.append(record[0].a)
+                    table[table.endIndex-1].b1.append(record[0].b1)
+                    table[table.endIndex-1].b2.append(record[0].b2)
+                    table[table.endIndex-1].rr.append(record[0].rr)
+                    table[table.endIndex-1].c.append(record[0].c)
+                    table[table.endIndex-1].re.append(record[0].re)
+                    table[table.endIndex-1].kar.append(record[0].kar)
+                    table[table.endIndex-1].mds.append(record[0].mds)
+                    table[table.endIndex-1].kr.append(record[0].kr)
+                    table[table.endIndex-1].te.append(record[0].te)
+                    table[table.endIndex-1].ok.append(record[0].ok)
+                    table[table.endIndex-1].ne.append(record[0].ne)
+                    record.remove(at: 0)
+                } else {
+                    table.append(TableRecord(day: record[0].day, time:[record[0].time], foodType: [record[0].foodType], food:[record[0].food], g:[record[0].g], carbo: [record[0].carbo], prot: [record[0].prot], fat: [record[0].fat], ec: [record[0].ec], gi: [record[0].gi], empty: [record[0].empty], water: [record[0].water], nzhk: [record[0].nzhk], hol: [record[0].hol], pv: [record[0].pv], zola: [record[0].zola], na: [record[0].na], k: [record[0].k], ca: [record[0].ca], mg: [record[0].mg], p: [record[0].p], fe: [record[0].fe], a: [record[0].a], b1: [record[0].b1], b2: [record[0].b2], rr: [record[0].rr], c: [record[0].c], re: [record[0].re], kar: [record[0].kar], mds: [record[0].mds], kr: [record[0].kr], te: [record[0].te], ok: [record[0].ok], ne: [record[0].ne]))
+                    record.remove(at: 0)
+                }
+            }
+            
         }
-        record.removeAll()
     }
     catch {
         print(error)
     }
     return table
 }
-
-// Помойка кода
-//        i = 1
-//        table.append(TableRecord(day: record[0].day, time: [record[0].time], foodType: [record[0].foodType], food: [record[0].food], g: [record[0].g]))
-//        while i < record.count {
-//            if record[i].day == table[i-1].day {
-//                table[i-1].time.append(record[i].time)
-//                table[i-1].foodType.append(record[i].foodType)
-//                table[i-1].food.append(record[i].food)
-//                table[i-1].g.append(record[i].g)
-//                record.remove(at: i)
-//            } else {
-//                table.append(TableRecord(day: record[i].day, time:[record[i].time], foodType: [record[i].foodType], food:[record[i].food], g:[record[i].g]))
-//                i += 1
-//            }
-//        }
-//        record.removeAll()
--- dia/foodTable.swift
+++ dia/foodTable.swift
@@ -7,23 +7,19 @@
 import Foundation
 import xlsxwriter
 
-class Anatomy{
+class Anatomy {
     func generate() async throws -> URL {
+        
         let documentDirectory = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor:nil, create:false)
         let fileURL = documentDirectory.appendingPathComponent("foodTable.xlsx")
         
-        //Create a new workbook.
-        //Ditch first 6 characters, because they are of the form file://
         let workbook = workbook_new((fileURL.absoluteString.dropFirst(6) as NSString).fileSystemRepresentation)
-        //Add a worksheet with a user defined sheet name.
         let worksheet1 = workbook_add_worksheet(workbook, "Приемы пищи")
-        //Widen the first column to make the text clearer.
         worksheet_set_column(worksheet1, 0, 0, 20, nil)
         let merge_format = workbook_add_format(workbook);
         format_set_align(merge_format, UInt8(LXW_ALIGN_CENTER.rawValue));
         format_set_align(merge_format, UInt8(LXW_ALIGN_VERTICAL_CENTER.rawValue));
         
-        //Write some unformatted data.
         let tbl = getFoodRecords()
         let formater = DateFormatter()
         formater.dateFormat = "dd.MM.yyyy"
@@ -44,28 +40,37 @@ class Anatomy{
         var d = 0
         var r = 0
         var r1 = 0
+        var rr = 0
         
-        print(listOfIndex)
-        print(listOfIndex1)
         for i in 0..<tbl.count {
             worksheet_merge_range(worksheet1, lxw_row_t(listOfIndex[0..<i+1].reduce(0,+)-listOfIndex[i]), 0, lxw_row_t(listOfIndex[0..<i+1].reduce(0,+)-1), 0, nil, nil);
             worksheet_write_string(worksheet1, lxw_row_t(d), 0, formater.string(from: tbl[i].day), merge_format)
             d = d + tbl[i].food.joined().count
             for i1 in 0..<tbl[i].time.count {
-                worksheet_write_string(worksheet1, lxw_row_t(r1), 1, formater1.string(from: tbl[i].time[i1]), nil)
-                worksheet_write_string(worksheet1, lxw_row_t(r1), 2, tbl[i].foodType[i1], nil)
+                worksheet_merge_range(worksheet1, lxw_row_t(r1), 1, lxw_row_t(r1+listOfIndex1[rr]-1), 1, nil, nil);
+                worksheet_write_string(worksheet1, lxw_row_t(r1), 1, formater1.string(from: tbl[i].time[i1]), merge_format)
+                worksheet_merge_range(worksheet1, lxw_row_t(r1), 2, lxw_row_t(r1+listOfIndex1[rr]-1), 2, nil, nil);
+                worksheet_write_string(worksheet1, lxw_row_t(r1), 2, tbl[i].foodType[i1], merge_format)
                 r1 = r1 + tbl[i].food[i1].count
+                rr += 1
             }
             for j in 0..<tbl[i].food.joined().count {
                 worksheet_write_string(worksheet1, lxw_row_t(r), 3, Array(tbl[i].food.joined())[j], nil)
                 worksheet_write_string(worksheet1, lxw_row_t(r), 4, Array(tbl[i].g.joined())[j], nil)
+                worksheet_write_string(worksheet1, lxw_row_t(r), 5, Array(tbl[i].carbo.joined())[j], nil)
+                worksheet_write_string(worksheet1, lxw_row_t(r), 6, Array(tbl[i].prot.joined())[j], nil)
+                worksheet_write_string(worksheet1, lxw_row_t(r), 7, Array(tbl[i].fat.joined())[j], nil)
+                worksheet_write_string(worksheet1, lxw_row_t(r), 8, Array(tbl[i].ec.joined())[j], nil)
+                worksheet_write_string(worksheet1, lxw_row_t(r), 9, Array(tbl[i].gi.joined())[j], nil)
+                worksheet_write_string(worksheet1, lxw_row_t(r), 10, Array(tbl[i].empty.joined())[j], nil)
+                worksheet_write_string(worksheet1, lxw_row_t(r), 11, Array(tbl[i].water.joined())[j], nil)
                 r += 1
             }
         }
-
-        //Close the workbook, save the file and free any memory.
+        
+//        worksheet_protect(worksheet1, "pass123", nil)
         let error = workbook_close(workbook)
-        //Check if there was any error creating the xlsx file.
+        
         if (error.rawValue != LXW_NO_ERROR.rawValue){
             print("Error in workbook_close().\nError %d = %s\n", error, lxw_strerror(error)!)
         }
