c1201e2bf520c7391cbf1cdb6c91db8814cd0ac3
Artem <amedi.ioakim@gmail.com>
2022-02-27 20:17:57 +0300

    27.02.2022

--- dia/ModalView.swift
+++ dia/ModalView.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct ModalView: View {
     @State var phelper : Bool = true
+    let fileUrl = Bundle.main.url(forResource: "Help", withExtension: "pdf")!
     var body: some View {
         NavigationView {
             ZStack {
@@ -26,8 +28,13 @@ struct ModalView: View {
                         NavigationLink(destination: poldny()) {
                             Button("Отметить полные дни", action: {})
                         }.foregroundColor(.black)
-                        Button("Обучение", action: {
-                        }).foregroundColor(.black)
+                        NavigationLink(destination: PDFKitView(url: fileUrl)
+                                        .navigationBarTitleDisplayMode(.inline)) {
+                            Button("Обучение", action: {})
+                        }.foregroundColor(.black)
+//                        Button("Обучение", action: {
+//                            PDFKitView(url: fileUrl)
+//                        }).foregroundColor(.black)
                         Button("Помощь", action: {
                             phelper = false
                         })
@@ -41,6 +48,31 @@ struct ModalView: View {
             }
             .navigationTitle("Дополнительно")
         }
+    }
+}
+
+struct PDFKitView: View {
+    var url: URL
+    var body: some View {
+        PDFKitRepresentedView(url)
+    }
+}
+
+struct PDFKitRepresentedView: UIViewRepresentable {
+    let url: URL
+    init(_ url: URL) {
+        self.url = url
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<PDFKitRepresentedView>) -> PDFKitRepresentedView.UIViewType {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: self.url)
+        pdfView.autoScales = true
+        return pdfView
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PDFKitRepresentedView>) {
+        // Update the view.
     }
 }
 
