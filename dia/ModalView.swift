b8633e86700403c7f931ebd3382b32ab136e6d15
Artem <amedi.ioakim@gmail.com>
2022-04-24 20:04:28 +0300

    24.04.22

--- dia/ModalView.swift
+++ dia/ModalView.swift
@@ -58,30 +58,6 @@ struct ModalView: View {
     }
 }
 
-struct PDFKitView: View {
-    var url: URL
-    var body: some View {
-        PDFKitRepresentedView(url)
-    }
-}
-
-struct PDFKitRepresentedView: UIViewRepresentable {
-    let url: URL
-    init(_ url: URL) {
-        self.url = url
-    }
-    
-    func makeUIView(context: UIViewRepresentableContext<PDFKitRepresentedView>) -> PDFKitRepresentedView.UIViewType {
-        let pdfView = PDFView()
-        pdfView.document = PDFDocument(url: self.url)
-        pdfView.autoScales = true
-        return pdfView
-    }
-    
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PDFKitRepresentedView>) {
-    }
-}
-
 struct ModalView_Previews: PreviewProvider {
     static var previews: some View {
         ModalView()
