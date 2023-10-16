deeba6c568c483891cd5a2324df4c8e31a65a78a
Artem <76273674+artemisak@users.noreply.github.com>
2023-10-16 22:57:01 +0300

    16.10.2023

--- dia/Registration/regHelper.swift
+++ dia/Registration/regHelper.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct regHelper: View {
     var body: some View {
         ScrollView {
-            Text("Для доступа к приложению по программе обязательного медицинского страхования обратитесь в ближайщее государственное лечебно-профилактическое учреждение согласно месту жительства.").foregroundColor(Color("listButtonColor")).padding()
+            Text("Для доступа к приложению по программе обязательного медицинского страхования обратитесь за пригласительным кодом в ближайщее государственное лечебно-профилактическое учреждение согласно месту жительства. И пройдите регистрацию по ссылке: http://diacompanion.ru/register.").foregroundColor(Color("listButtonColor")).padding()
         }
         .navigationTitle("Помощь")
     }
